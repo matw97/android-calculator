d90362f9051b76453897b371910efa2ebd1fd69a
Mateusz Wiśnik <wisnikmateusz@gmail.com>
2018-11-16 22:05:26 +0100

    Inital commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Simple calculator app, made for university classes
---
+++ app/src/main/java/com/example/mateusz/calculator/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.mateusz.calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void add(View view) {
+        EditText firstInput = findViewById(R.id.firstNumber);
+        EditText secondInput = findViewById(R.id.secondNumber);
+        TextView resultView = findViewById(R.id.result);
+        int firstNumber =  Integer.parseInt(firstInput.getText().toString());
+        int secondNumber = Integer.parseInt(secondInput.getText().toString());
+        int result = firstNumber + secondNumber;
+
+        resultView.setText(String.valueOf(result));
+    }
+
+    public void subtract(View view) {
+        EditText firstInput = findViewById(R.id.firstNumber);
+        EditText secondInput = findViewById(R.id.secondNumber);
+        TextView resultView = findViewById(R.id.result);
+
+        int firstNumber = Integer.parseInt(firstInput.getText().toString());
+        int secondNumber =  Integer.parseInt(secondInput.getText().toString());
+        int result = firstNumber - secondNumber;
+
+        resultView.setText(String.valueOf(result));
+    }
+
+    public void multiply(View view) {
+        EditText firstInput = findViewById(R.id.firstNumber);
+        EditText secondInput = findViewById(R.id.secondNumber);
+        TextView resultView = findViewById(R.id.result);
+
+        int firstNumber = Integer.parseInt(firstInput.getText().toString());
+        int secondNumber =  Integer.parseInt(secondInput.getText().toString());
+        int result = firstNumber * secondNumber;
+
+        resultView.setText(String.valueOf(result));
+    }
+
+    public void divide(View view) {
+        EditText firstInput = findViewById(R.id.firstNumber);
+        EditText secondInput = findViewById(R.id.secondNumber);
+        TextView resultView = findViewById(R.id.result);
+
+        int firstNumber = Integer.parseInt(firstInput.getText().toString());
+        int secondNumber =  Integer.parseInt(secondInput.getText().toString());
+        int result;
+        try {
+            result = firstNumber / secondNumber;
+            resultView.setText(String.valueOf(result));
+        } catch (Exception e) {
+            resultView.setText("You can't divide by 0!");
+        }
+    }
+}
